12c8a2b10077e4a63fedd85e2feda729b36e2c8f
Awa Melvine <melvineawa@gmail.com>
2019-07-08 12:23:41 +0100

    Create find all users endpoint

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require("express");
+const User = require("./data/db");
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/users", (req, res) => {
+  User.find()
+    .then(users => {
+      res.status(200).json({ data: users });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.listen(8080, () => console.log("Server running at localhost:8080"));
