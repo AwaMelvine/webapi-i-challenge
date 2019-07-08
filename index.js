349aba293a77f027cf446e5e15e7c7582d8ea90e
Awa Melvine <melvineawa@gmail.com>
2019-07-08 12:58:51 +0100

    Return a 404 error when user to delete not found

--- index.js
+++ index.js
@@ -46,7 +46,9 @@ app.post("/api/users", (req, res) => {
       res.status(201).json({ data: id });
     })
     .catch(error => {
-      console.log(error);
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
     });
 });
 
@@ -66,6 +68,11 @@ app.delete("/api/users/:id", (req, res) => {
   const { id } = req.params;
   User.remove(id)
     .then(count => {
+      if (!count) {
+        return res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
       res.status(200).json({ data: count });
     })
     .catch(error => {
