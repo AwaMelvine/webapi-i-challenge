0204d2bf6b2fe23fd6bf3cf12f4e3a67fcce0b21
Awa Melvine <melvineawa@gmail.com>
2019-07-08 13:11:29 +0100

    Add cors middleware

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const User = require("./data/db");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/users", (req, res) => {
   User.find()
